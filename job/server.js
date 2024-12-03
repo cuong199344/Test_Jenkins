6b729dadae69b6befee63d293e17f0e39cdc01e8
cuong199344 <cuonghaxer2003@gmail.com>
2024-12-03 15:47:21 +0700

    test new jenkins

--- job/server.js
+++ job/server.js
@@ -7,7 +7,7 @@ dbConnection();
 const PORT = process.env.PORT || 8800;
 
 const server = app.listen(PORT, () => {
-  console.log(`Dev Server running on port!!!!!!!!!!!: ${PORT}`);
+  console.log(`Dev Server running on port!!!!!!!!!!!!!: ${PORT}`);
 });
 
 export default server;
